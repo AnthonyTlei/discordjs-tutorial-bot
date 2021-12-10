c18b9b13df18c98b87ce61eccf957fd1a986978b
Anthony Tleiji <anthonytleiji@gmail.com>
2021-12-10 15:32:38 +0200

    Integrated WOKCommands as command handler

---
+++ commands/ping.ts
@@ -0,0 +1,29 @@
+import { ICommand } from "wokcommands";
+
+export default {
+
+    category: 'Testing',
+    description: 'Replies with pong',
+    slash: 'both', // For Slash and Legacy commands
+    testOnly: true, // For guild specific commands (not global)
+
+    /* message is used for legacy commands while interaction is for Slash commands 
+        interaction will be null if the user used legacy commands
+        message will be null if the user used slash commands
+    */
+    callback: ({message, interaction}) => {
+        
+        /* WOKCommands will handle cecking if its a legacy or slash command */
+        // if(message){
+        //     message.reply('pong')
+        // }
+
+        // if(interaction){
+        //     interaction.reply('pong')
+        // }
+
+        return 'Pong'
+
+    }
+
+} as ICommand
