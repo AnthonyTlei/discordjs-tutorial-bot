a44c6267d8152b8bbf6e987771c430fa65942056
Anthony Tleiji <anthonytleiji@gmail.com>
2021-12-10 15:13:00 +0200

    Added Slash Commands Added Add Function

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -36,13 +45,71 @@ const client = new discord_js_1.default.Client({
     ]
 });
 client.on('ready', () => {
+    var _a;
     console.log('The bot is ready');
+    /* We have 2 types of Slash commands:
+        - Guild slash commands: Registered to a specific guild, useful for development
+        - Global slash commands: Regitsered to all guilds that the bot is in (Might take up to an hour to become visible to all guilds)
+        So we use guild commands until we know everything is woring then we switch to global commands
+    */
+    // Remove the guildID to turn make commands global
+    const guildID = '760797300464943114';
+    const guild = client.guilds.cache.get(guildID);
+    let commands; // Holds the application command manager (guild or global)
+    if (guild) {
+        commands = guild.commands;
+    }
+    else {
+        commands = (_a = client.application) === null || _a === void 0 ? void 0 : _a.commands;
+    }
+    // Registering Commands 
+    commands === null || commands === void 0 ? void 0 : commands.create({
+        name: 'ping',
+        description: 'Replies with pong'
+    });
+    commands === null || commands === void 0 ? void 0 : commands.create({
+        name: 'add',
+        description: 'Adds two numbers',
+        options: [
+            {
+                name: 'num1',
+                description: 'the first number',
+                required: true,
+                type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: 'the second number',
+                required: true,
+                type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.NUMBER
+            }
+        ]
+    });
 });
-client.on('messageCreate', (message) => {
-    if (message.content.toLowerCase() === 'ping') {
-        message.reply({
-            content: 'pong'
+client.on('interactionCreate', (interaction) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!interaction.isCommand())
+        return;
+    const { commandName, options } = interaction;
+    if (commandName.toLowerCase() === 'ping') {
+        interaction.reply({
+            content: 'pong',
+            ephemeral: true // Only the user who ran the command can see the reply
         });
     }
-});
+    else if (commandName.toLowerCase() === 'add') {
+        const num1 = options.getNumber('num1');
+        const num2 = options.getNumber('num2');
+        // we know these values are not null becauuse we set them as required, so we added the '!'
+        /* we use deferring because by default we only have 3 seconds to reply but some calculations require more time */
+        yield interaction.deferReply({
+            ephemeral: true
+        });
+        /* Because we deferred the reply we use editReply instead of Reply
+            We also added an await because the return of editReply is a Promise so we if had code after it we need to wait
+        */
+        yield interaction.editReply({
+            content: `The sum is ${num1 + num2}`
+        });
+    }
+}));
 client.login(process.env.TOKEN);
--- index.ts
+++ index.ts
@@ -15,14 +15,79 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log('The bot is ready')
+
+    /* We have 2 types of Slash commands: 
+        - Guild slash commands: Registered to a specific guild, useful for development
+        - Global slash commands: Regitsered to all guilds that the bot is in (Might take up to an hour to become visible to all guilds)
+        So we use guild commands until we know everything is woring then we switch to global commands
+    */
+
+    // Remove the guildID to turn make commands global
+    const guildID = '760797300464943114'
+    const guild = client.guilds.cache.get(guildID)
+    let commands // Holds the application command manager (guild or global)
+
+    if(guild){
+        commands = guild.commands
+    } else{
+        commands = client.application?.commands
+    }
+
+    // Registering Commands 
+    commands?.create({
+        name: 'ping',
+        description: 'Replies with pong'
+    })
+
+    commands?.create({
+        name: 'add',
+        description: 'Adds two numbers',
+        options: [
+            {
+                name: 'num1',
+                description: 'the first number',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: 'the second number',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            }
+        ]
+    })
 })
 
-client.on('messageCreate', (message) => {
-    if(message.content.toLowerCase() === 'ping'){
-        message.reply({
-            content: 'pong'
+client.on('interactionCreate', async (interaction) => {
+
+    if(!interaction.isCommand()) return
+
+    const {commandName, options} = interaction
+
+    if(commandName.toLowerCase() === 'ping'){
+        interaction.reply({
+            content: 'pong',
+            ephemeral: true // Only the user who ran the command can see the reply
+        })
+    }else if (commandName.toLowerCase() === 'add'){
+        const num1 = options.getNumber('num1')!
+        const num2 = options.getNumber('num2')! 
+        // we know these values are not null becauuse we set them as required, so we added the '!'
+
+        /* we use deferring because by default we only have 3 seconds to reply but some calculations require more time */
+        await interaction.deferReply({
+            ephemeral: true
+        })
+
+        /* Because we deferred the reply we use editReply instead of Reply 
+            We also added an await because the return of editReply is a Promise so we if had code after it we need to wait
+        */ 
+        await interaction.editReply({
+            content: `The sum is ${num1 + num2}`
         })
     }
+
 })
 
 client.login(process.env.TOKEN)
