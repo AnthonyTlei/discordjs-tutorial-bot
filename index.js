0a2ffb7e275c18f511cf54469557e7a2a78c7052
Anthony Tleiji <anthonytleiji@gmail.com>
2021-12-10 11:34:09 +0200

    Added continuous TS conversion and PM2 Running

---
+++ index.js
@@ -0,0 +1,48 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = __importStar(require("discord.js"));
+const dotenv_1 = __importDefault(require("dotenv"));
+// Give us access to the variables inside the .env file as environment variables
+dotenv_1.default.config();
+const client = new discord_js_1.default.Client({
+    // Discord V13 forces us to state our Intents for the bot
+    // Intents are a way to tell discord what our bot intents to use and what information it needs
+    // You should not subscribe to all events, only pick the ones needed
+    intents: [
+        discord_js_1.Intents.FLAGS.GUILDS,
+        discord_js_1.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+client.on('ready', () => {
+    console.log('The bot is ready');
+});
+client.on('messageCreate', (message) => {
+    if (message.content.toLowerCase() === 'ping') {
+        message.reply({
+            content: 'pong'
+        });
+    }
+});
+client.login(process.env.TOKEN);
